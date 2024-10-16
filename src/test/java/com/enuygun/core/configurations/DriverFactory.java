39a44896afc0e64ddb03f36f88c563ad672367c7
YusufCoskun23 <ysfknncskn@gmail.com>
2024-10-16 13:46:33 +0300

    driver quit

--- src/test/java/com/enuygun/core/configurations/DriverFactory.java
+++ src/test/java/com/enuygun/core/configurations/DriverFactory.java
@@ -33,8 +33,8 @@ public class DriverFactory {
 
     public void quitWebDriver() {
         if (null != driver) {
-            //driver.get().quit();
-            //driver.remove();
+            driver.get().quit();
+            driver.remove();
         }
     }
 
